8de2f5c24a2e8cee76bd37480eb594b131e1d4d4
ishworX <git.ishwor@gmail.com>
2024-07-14 23:34:17 -0600

    checking new design

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,28 +22,6 @@ function ActionButtons() {
 
 export const Hero = () => {
     return (
-        <div className='h-[calc(100vh-13vh)] w-full flex justify-center flex-col'>
-            <div className='w-4/5 mx-auto items-center grid grid-cols-1 lg:grid-cols-5 gap-8'>
-            {/* Text Content */}
-            <div className='col-span-2'>
-                <h1 className='text-2xl sm:text-4xl lg:text-5xl xl:text-7xl text-blue-950 font-bold'>
-                    This will be the title!
-                </h1>
-                <p className='text-sm md:text-[17px] text-gray-700 text-opacity-80 mt-4 font-medium'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus magni, deleniti quis tempora consectetur eligendi magnam tenetur enim saepe voluptates libero alias placeat aliquam odit ut sit deserunt laborum fuga ipsum. Cum, ad maxime corrupti possimus quos eum aliquid sapiente perspiciatis minus modi inventore natus provident ullam itaque sit similique.
-                </p>
-                <div className='flex items-center space-x-2 mt-10'>
-                    <ActionButtons />
-                </div>
-            </div>
-            {/* Image Content */}
-            <div>
-                <Image src="/hero.png" alt="hero" width={700} height={700}/>
-
-            </div>
-
-            </div>
-
-        </div>
+        // Trying new section
     )
 }
